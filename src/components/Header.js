399a16852da1da37d82ae05b342de4e1e3df7890
mac rent <macrent@macs-air-2.home>
2021-06-20 14:47:27 +1000

    first commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,18 +4,15 @@ import React from 'react'
 const Header = props => (
   <header id="header" style={props.timeout ? { display: 'none' } : {}}>
     <div className="logo">
-      <span className="icon fa-diamond"></span>
+      <span className="icon fas fa-code"></span>
     </div>
     <div className="content">
       <div className="inner">
-        <h1>Dimension</h1>
+        <h1>Katie Lugton</h1>
+        <p>Junior Web Developer</p>
+        <p>What's up!</p>
         <p>
-          A fully responsive site template designed by{' '}
-          <a href="https://html5up.net">HTML5 UP</a> and released
-          <br />
-          for free under the{' '}
-          <a href="https://html5up.net/license">Creative Commons</a> license.
-        </p>
+      </p>
       </div>
     </div>
     <nav>
@@ -23,28 +20,19 @@ const Header = props => (
         <li>
           <button
             onClick={() => {
-              props.onOpenArticle('intro')
-            }}
-          >
-            Intro
-          </button>
-        </li>
-        <li>
-          <button
-            onClick={() => {
-              props.onOpenArticle('work')
-            }}
-          >
-            Work
-          </button>
-        </li>
-        <li>
-          <button
-            onClick={() => {
               props.onOpenArticle('about')
             }}
           >
             About
+          </button>
+        </li>
+        <li>
+          <button
+            onClick={() => {
+              props.onOpenArticle('projects')
+            }}
+          >
+            Projects
           </button>
         </li>
         <li>
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,9 @@
 import PropTypes from 'prop-types'
 import React from 'react'
-import pic01 from '../images/pic01.jpg'
+// import pic01 from '../images/pic01.jpg'
 import pic02 from '../images/pic02.jpg'
-import pic03 from '../images/pic03.jpg'
+// import pic03 from '../images/pic03.jpg'
+import me from '../images/me.jpg'
 
 class Main extends React.Component {
   render() {
@@ -22,47 +23,45 @@ class Main extends React.Component {
         style={this.props.timeout ? { display: 'flex' } : { display: 'none' }}
       >
         <article
-          id="intro"
-          className={`${this.props.article === 'intro' ? 'active' : ''} ${
+          id="about"
+          className={`${this.props.article === 'about' ? 'active' : ''} ${
             this.props.articleTimeout ? 'timeout' : ''
           }`}
           style={{ display: 'none' }}
         >
-          <h2 className="major">Intro</h2>
+          <h2 className="major">About</h2>
           <span className="image main">
-            <img src={pic01} alt="" />
+            <img src={me} alt="funny" />
           </span>
           <p>
-            Aenean ornare velit lacus, ac varius enim ullamcorper eu. Proin
-            aliquam facilisis ante interdum congue. Integer mollis, nisl amet
-            convallis, porttitor magna ullamcorper, amet egestas mauris. Ut
-            magna finibus nisi nec lacinia. Nam maximus erat id euismod egestas.
-            By the way, check out my <a href="#work">awesome work</a>.
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
-            dapibus rutrum facilisis. Class aptent taciti sociosqu ad litora
-            torquent per conubia nostra, per inceptos himenaeos. Etiam tristique
-            libero eu nibh porttitor fermentum. Nullam venenatis erat id
-            vehicula viverra. Nunc ultrices eros ut ultricies condimentum.
-            Mauris risus lacus, blandit sit amet venenatis non, bibendum vitae
-            dolor. Nunc lorem mauris, fringilla in aliquam at, euismod in
-            lectus. Pellentesque habitant morbi tristique senectus et netus et
-            malesuada fames ac turpis egestas. In non lorem sit amet elit
-            placerat maximus. Pellentesque aliquam maximus risus, vel sed
-            vehicula.
+          Hi, I'm Katie and I'm very excited to be a Junior Software Developer!
+          <br/>
+          <br/>
+          I was previously working in the legal industry for over 8 years and reached a point in my career where the service I provided was evolving. I noticed aspects of my role being replaced with programs and more and more red tape giving me less automony to problem solve and forward think. I spent time traveling and pursuing other interests alongside my regular job in order to feel into what might be the best way forward for me. I spent time in Australia and overseas learning, traveling and teaching Pilates - becoming more self aware, growing up .. all the fun stuff.
+          <br/>
+          <br/>
+          I eventually realised that software development - in its essence, was exactly what I had always felt a passion for! <br/>Pilates - hacking and understanding the anatomy to improve my own user experience.
+          <br/>Legal - organising and supporting individuals in the judicial system, understanding situational needs and their impacts on desigining and implementing seemless processes. The importance of risk management in both pursuits. It just made sense and intuitively felt right. I see coding as learning to read and write but in a language that has a greater impact in supporting people and society.
+          <br/>
+          <br/>
+          I began my journey learning code in 2020. I moved close to University, completed a bridging course and started a computer science degree. Once covid lockdowns hit I knew I would benefit from going down the bootcamp route first, with face to face learning and a stronger teaching experience. I completed my General Assembly Software Engineering Immersive course in June 2021 receiving the Han Shan award for knowing clearly what was really going on and came out of it with 10x more confidence to further teach myself.
+          <br/>
+          <br/>
+          I would still like to complete a computer science degree to understand industry standards and have a holistic understanding of the tech industry, but for now I am looking to grow the fundamental knowledge I do have in the real world of software development.
+          <br/>
+          <br/>
+          Thank you for taking the time to read about my journey and get to know me a little more. As time progresses I will endeavour to dazzle you with my ability and this will be a distant memory but for now it's what you should know about me and my commitment to becoming an incredible software developer.
           </p>
           {close}
         </article>
-
         <article
-          id="work"
-          className={`${this.props.article === 'work' ? 'active' : ''} ${
+          id="projects"
+          className={`${this.props.article === 'projects' ? 'active' : ''} ${
             this.props.articleTimeout ? 'timeout' : ''
           }`}
           style={{ display: 'none' }}
         >
-          <h2 className="major">Work</h2>
+          <h2 className="major">Projects</h2>
           <span className="image main">
             <img src={pic02} alt="" />
           </span>
@@ -84,28 +83,7 @@ class Main extends React.Component {
           {close}
         </article>
 
-        <article
-          id="about"
-          className={`${this.props.article === 'about' ? 'active' : ''} ${
-            this.props.articleTimeout ? 'timeout' : ''
-          }`}
-          style={{ display: 'none' }}
-        >
-          <h2 className="major">About</h2>
-          <span className="image main">
-            <img src={pic03} alt="" />
-          </span>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur et adipiscing elit. Praesent
-            eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam
-            erat volutpat. Praesent urna nisi, fringila lorem et vehicula
-            lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices.
-            Aliquam libero et malesuada fames ac ante ipsum primis in faucibus.
-            Cras viverra ligula sit amet ex mollis mattis lorem ipsum dolor sit
-            amet.
-          </p>
-          {close}
-        </article>
+
 
         <article
           id="contact"
@@ -115,50 +93,27 @@ class Main extends React.Component {
           style={{ display: 'none' }}
         >
           <h2 className="major">Contact</h2>
-          <form method="post" action="#">
-            <div className="field half first">
-              <label htmlFor="name">Name</label>
-              <input type="text" name="name" id="name" />
-            </div>
-            <div className="field half">
-              <label htmlFor="email">Email</label>
-              <input type="text" name="email" id="email" />
-            </div>
-            <div className="field">
-              <label htmlFor="message">Message</label>
-              <textarea name="message" id="message" rows="4"></textarea>
-            </div>
-            <ul className="actions">
-              <li>
-                <input type="submit" value="Send Message" className="special" />
-              </li>
-              <li>
-                <input type="reset" value="Reset" />
-              </li>
-            </ul>
-          </form>
+          <h3> Email me! klugton@hotmail.com </h3>
           <ul className="icons">
             <li>
               <a
-                href="https://twitter.com/HuntaroSan"
+                href="https://www.linkedin.com/in/katielugton/"
+                className="icon fab fa-linkedin"
+              >
+                <span className="label">LinkedIn</span>
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://twitter.com/KatieZoePink"
                 className="icon fa-twitter"
               >
                 <span className="label">Twitter</span>
               </a>
             </li>
             <li>
-              <a href="https://codebushi.com" className="icon fa-facebook">
-                <span className="label">Facebook</span>
-              </a>
-            </li>
-            <li>
-              <a href="https://codebushi.com" className="icon fa-instagram">
-                <span className="label">Instagram</span>
-              </a>
-            </li>
-            <li>
               <a
-                href="https://github.com/codebushi/gatsby-starter-dimension"
+                href="https://github.com/KatieZoe"
                 className="icon fa-github"
               >
                 <span className="label">GitHub</span>
